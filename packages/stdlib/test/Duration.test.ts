6c14088ef57f19f57d411f3df3bc68895e91c117
Michael Arnaldi <arnaldimichael@gmail.com>
2022-03-24 00:20:32 +0000

    chore: avoid newlines

--- packages/stdlib/test/Duration.test.ts
+++ packages/stdlib/test/Duration.test.ts
@@ -3,20 +3,17 @@ import { assert, describe, it } from "vitest";
 describe("Duration", () => {
   it("equals", () => {
     assert.isTrue(
-      (1).hours
-        == (60).minutes
+      (1).hours == (60).minutes
     );
   });
   it("times", () => {
     assert.isTrue(
-      (1).minutes
-        == (1).seconds * 60
+      (1).minutes == (1).seconds * 60
     );
   });
   it("add", () => {
     assert.isTrue(
-      (1).minutes
-        == (30).seconds + (30).seconds
+      (1).minutes == (30).seconds + (30).seconds
     );
   });
   it(">", () => {
