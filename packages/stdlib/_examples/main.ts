9e93e4d13a0e1a58229b1cc0862b0dca41734ad7
Michael Arnaldi <arnaldimichael@gmail.com>
2022-03-29 21:50:11 +0000

    chore: improve pipeable example

--- packages/stdlib/_examples/main.ts
+++ packages/stdlib/_examples/main.ts
@@ -1,6 +1,11 @@
 console.log(ImmutableArray.from(List(0, 1, 2)) == ImmutableArray(0, 1, 2));
 console.log([0, 1, 2].immutable() == ImmutableArray(0, 1, 2));
-console.log(Option(0)(Option.$.map((n) => n + 1)) == Option(1));
+console.log(
+  Option(0)(
+    Option.$.map((n) => n + 1),
+    Option.$.map((n) => `res: ${n}`)
+  ) == Option("res: 1")
+);
 
 console.log(List(0, 1, 2) == 0 + (1 + List(2)));
 console.log(List(0, 1, 2) == List(0, 1) + 2);
