06d313a764c05766cb7f006bf26fa5998edbff38
Michael Arnaldi <arnaldimichael@gmail.com>
2022-04-06 14:42:35 +0000

    chore: fix script

--- .github/changeset-version.cjs
+++ .github/changeset-version.cjs
@@ -1,4 +1,4 @@
 const { execSync } = require("node:child_process");
 
-execSync("yarn changeset version");
-execSync("yarn");
+execSync("npx changeset version");
+execSync("YARN_ENABLE_IMMUTABLE_INSTALLS=false node .yarn/releases/yarn-3.1.1.cjs");
