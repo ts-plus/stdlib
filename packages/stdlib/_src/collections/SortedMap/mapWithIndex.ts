f5783696d0f5c3c395f27584ab206f8c9edce775
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-04-07 17:39:14 +0000

    feat(@tsplus/stdlib): add map, mapWithIndex, reduce, and reduceWithIndex to SortedMap

---
+++ packages/stdlib/_src/collections/SortedMap/mapWithIndex.ts
@@ -0,0 +1,14 @@
+/**
+ * @tsplus fluent SortedMap mapWithIndex
+ */
+export function mapWithIndex_<K, V, A>(
+  self: SortedMap<K, V>,
+  f: (k: K, v: V) => A
+): SortedMap<K, A> {
+  return self.reduceWithIndex(SortedMap.make<K, A>(self.getOrd()), (b, k, v) => b.set(k, f(k, v)));
+}
+
+/**
+ * @tsplus static SortedMap/Aspects mapWithIndex
+ */
+export const mapWithIndex = Pipeable(mapWithIndex_);
