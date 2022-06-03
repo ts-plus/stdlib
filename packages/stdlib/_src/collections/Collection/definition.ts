b72330ce761b919680d4dff2d497c37114828504
Michael Arnaldi <arnaldimichael@gmail.com>
2022-06-03 10:34:13 +0000

    feat(@tsplus/stdlib): Extend Iterable in Array/Set/Map to make it a Collection

--- packages/stdlib/_src/collections/Collection/definition.ts
+++ packages/stdlib/_src/collections/Collection/definition.ts
@@ -3,6 +3,11 @@ declare global {
    * @tsplus type Collection
    */
   export interface Iterable<T> {}
+
+  export interface Map<K, V> extends Iterable<[K, V]> {}
+  export interface Set<T> extends Iterable<T> {}
+  export interface Array<T> extends Iterable<T> {}
+  export interface ReadonlyArray<T> extends Iterable<T> {}
 }
 
 export type Collection<A> = ESIterable<A>
