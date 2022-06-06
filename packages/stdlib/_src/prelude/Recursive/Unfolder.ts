d2bcda2b5b70310253c6eb58c538d866e605dcfa
Will Alexander <walexander@mco.com>
2022-06-06 09:25:09 -0400

    feat(@stdlib/Recursive.foldDown): add foldDown method

--- packages/stdlib/_src/prelude/Recursive/Unfolder.ts
+++ packages/stdlib/_src/prelude/Recursive/Unfolder.ts
@@ -3,8 +3,12 @@
  * @tsplus companion Recursive/Unfolder/Ops
  */
 export class Unfolder<F extends HKT, A> {
-  constructor(readonly fn: Unfolder.Fn<F, A>){}
+  constructor(readonly fn: Unfolder.Fn<F, A>) {}
 }
 export declare namespace Unfolder {
+  /**
+   * A function to _generate_ a single value of a recursive structure from a seed-value
+   * aka _anamorphism_
+   */
   export type Fn<F extends HKT, A> = (f: A) => HKT.Kind<F, unknown, never, A>
 }
