27da7cc6073a050c7acb0101ea38365e72e27ba2
Wessel van der Veen <wesselvanderveen@gmail.com>
2022-06-09 13:46:07 +0200

    style(List): remove redundant if/else constructs

--- packages/stdlib/_src/collections/List/drop.ts
+++ packages/stdlib/_src/collections/List/drop.ts
@@ -6,20 +6,22 @@
 export function drop_<A, B>(self: List<A>, n: number): List<A> {
   if (n <= 0) {
     return self
-  } else if (n >= self.length()) {
-    return List.nil<A>()
-  } else {
-    let these = self
-
-    let i = 0
-    while (!these.isNil() && i < n) {
-      these = these.tail
-
-      i += 1
-    }
-
-    return these
   }
+
+  if (n >= self.length()) {
+    return List.nil<A>()
+  }
+
+  let these = self
+
+  let i = 0
+  while (!these.isNil() && i < n) {
+    these = these.tail
+
+    i += 1
+  }
+
+  return these
 }
 
 export const drop = Pipeable(drop_)
