aeb8e2915d6cbe9b1cbe3fbfb5c8665b617dc697
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-07-11 11:56:54 +0000

    feat(@tsplus/stdlib): update Random implementation

---
+++ packages/stdlib/_src/io/Random.ts
@@ -0,0 +1,6 @@
+// codegen:start {preset: barrel, include: ./Random/*.ts, prefix: "@tsplus/stdlib/io"}
+export * from "@tsplus/stdlib/io/Random/Distribution"
+export * from "@tsplus/stdlib/io/Random/Generator"
+export * from "@tsplus/stdlib/io/Random/MutableRandom"
+export * from "@tsplus/stdlib/io/Random/PCGRandom"
+// codegen:end
---
+++ packages/stdlib/_src/io/Random/Distribution/operations.ts
@@ -0,0 +1,5 @@
+// codegen:start {preset: barrel, include: ./operations/*.ts, prefix: "@tsplus/stdlib/io/Random/Distribution"}
+export * from "@tsplus/stdlib/io/Random/Distribution/operations/uniformArrayIntDistribution"
+export * from "@tsplus/stdlib/io/Random/Distribution/operations/uniformBigIntDistribution"
+export * from "@tsplus/stdlib/io/Random/Distribution/operations/uniformIntDistribution"
+// codegen:end
